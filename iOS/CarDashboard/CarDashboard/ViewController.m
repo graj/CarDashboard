7f5b100b151533c66c419fab68af188faa274a19
Christoph Pageler <christoph.pageler@ewetel.net>
2013-02-19 19:54:48 +0100

    Initial Project Comit

---
+++ iOS/CarDashboard/CarDashboard/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  CarDashboard
+//
+//  Created by Christoph Pageler on 19.02.13.
+//  Copyright (c) 2013 Christoph Pageler. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
